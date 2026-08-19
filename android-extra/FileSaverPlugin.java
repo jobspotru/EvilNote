cd536f4fb4894e7867f695555b1e4d21021dd84f
jobspotru <jobspotru@gmail.com>
2026-08-19 09:16:09 +0300

    Create FileSaverPlugin.java

---
+++ android-extra/FileSaverPlugin.java
@@ -0,0 +1,47 @@
+package com.evilnote.filesaver;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+@CapacitorPlugin(name = "FileSaver")
+public class FileSaverPlugin extends Plugin {
+
+    @PluginMethod
+    public void saveToDownloads(PluginCall call) {
+        String name = call.getString("name");
+        String content = call.getString("content");
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Downloads.DISPLAY_NAME, name);
+                values.put(MediaStore.Downloads.MIME_TYPE, "application/octet-stream");
+                Uri uri = getContext().getContentResolver().insert(MediaStore.Downloads.CONTENT_URI, values);
+                OutputStream os = getContext().getContentResolver().openOutputStream(uri);
+                os.write(content.getBytes("UTF-8"));
+                os.close();
+            } else {
+                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File f = new File(dir, name);
+                FileOutputStream fos = new FileOutputStream(f);
+                fos.write(content.getBytes("UTF-8"));
+                fos.close();
+            }
+            JSObject ret = new JSObject();
+            ret.put("path", name);
+            call.resolve(ret);
+        } catch (Exception e) {
+            call.reject(e.getMessage());
+        }
+    }
+}
