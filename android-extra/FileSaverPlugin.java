394695a51fe7d45680eeca5cd85f6896f3faf117
jobspotru <jobspotru@gmail.com>
2026-08-19 12:37:49 +0300

    Update FileSaverPlugin.java

--- android-extra/FileSaverPlugin.java
+++ android-extra/FileSaverPlugin.java
@@ -1,6 +1,7 @@
 package com.evilnote.filesaver;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -10,9 +11,13 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @CapacitorPlugin(name = "FileSaver")
 public class FileSaverPlugin extends Plugin {
@@ -39,6 +44,53 @@ public class FileSaverPlugin extends Plugin {
             }
             JSObject ret = new JSObject();
             ret.put("path", name);
+            call.resolve(ret);
+        } catch (Exception e) {
+            call.reject(e.getMessage());
+        }
+    }
+
+    @PluginMethod
+    public void listDownloads(PluginCall call) {
+        try {
+            JSONArray arr = new JSONArray();
+            Cursor c = getContext().getContentResolver().query(
+                Uri.parse("content://media/external/downloads"),
+                new String[] { "_id", "_display_name" },
+                null, null, "date_added DESC");
+            if (c != null) {
+                while (c.moveToNext()) {
+                    String name = c.getString(1);
+                    if (name != null && name.endsWith(".en")) {
+                        long id = c.getLong(0);
+                        JSONObject o = new JSONObject();
+                        o.put("name", name);
+                        o.put("uri", "content://media/external/downloads/" + id);
+                        arr.put(o);
+                    }
+                }
+                c.close();
+            }
+            JSObject ret = new JSObject();
+            ret.put("files", arr);
+            call.resolve(ret);
+        } catch (Exception e) {
+            call.reject(e.getMessage());
+        }
+    }
+
+    @PluginMethod
+    public void readUri(PluginCall call) {
+        String uri = call.getString("uri");
+        try {
+            InputStream is = getContext().getContentResolver().openInputStream(Uri.parse(uri));
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] buf = new byte[8192];
+            int r;
+            while ((r = is.read(buf)) != -1) bos.write(buf, 0, r);
+            is.close();
+            JSObject ret = new JSObject();
+            ret.put("content", bos.toString("UTF-8"));
             call.resolve(ret);
         } catch (Exception e) {
             call.reject(e.getMessage());
